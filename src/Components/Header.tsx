7301a3ec0f03c3f706cdbde6eacc6d087948aad8
Tyla <tyla.ono@gmail.com>
2024-03-04 00:38:17 +1000

    Add Header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom"
 function Header() {
     return(
-        <>
-            <Link to={'/'}>MY SHOP</Link>
-        </>
+        <header className="flex justify-center items-center bg-background bg-opacity-90 px-6 py-4 border-b-2 border-neutral-500 gap-10">
+            <Link  to={'/'} className="max-w-[200px]">
+                <img src="./src/assets/images/amazone-logo.png" alt="amazone logo" />
+            </Link>
+            {/* todo: Add search bar to filter shop */}
+            {/* <input className="min-w-[200px] max-w-[800px] grow rounded-full min-h-[30px] max-h-[60px] justify-self-center" type="text" /> */}
+        </header>
     )
 }
 
