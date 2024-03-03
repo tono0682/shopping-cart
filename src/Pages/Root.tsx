6e4eb6ea94ba79bc5c5e757ab4e43650dca58c4e
Tyla <tyla.ono@gmail.com>
2024-03-04 00:41:11 +1000

    Update root page

--- src/Pages/Root.tsx
+++ src/Pages/Root.tsx
@@ -1,12 +1,18 @@
 import { Outlet} from "react-router-dom";
 import Header from "../Components/Header";
+import NavBar from "../Components/NavBar";
+import Footer from "../Components/Footer";
 
 export default function Root() {
+    
     return (
-        <>
+        <div className="flex flex-col min-h-screen bg-jungle-bg bg-cover">
             <Header />
-            <Outlet />
-            <footer>Footer</footer>
-        </>
+            <NavBar />
+            <div className="grow bg-gray-100 bg-opacity-100 flex flex-col py-5 px-10" >
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
     )
 }
