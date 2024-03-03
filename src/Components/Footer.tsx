f56872054d8d2476f616fd9833692f76f50164fe
Tyla <tyla.ono@gmail.com>
2024-03-04 00:40:04 +1000

    Add footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return(
+        <footer className="bg-background bg-opacity-90 flex justify-center items-center gap-20 text-secondary py-2 h-16">
+                <p>Email: customer-support@amazone.com</p>
+                <p>Phone: 1234-5678</p>
+        </footer>
+    )
+}
+
+export default Footer
