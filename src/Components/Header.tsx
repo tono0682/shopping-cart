0b42b58e7676a2fd395feea79761765af186ecdc
Tyla <tyla.ono@gmail.com>
2024-03-04 12:04:13 +1000

    Create dis folder and update reference

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ function Header() {
     return(
         <header className="flex justify-center items-center bg-background bg-opacity-90 px-6 py-4 border-b-2 border-neutral-500 gap-10">
             <Link  to={'/'} className="max-w-[200px]">
-                <img src="/public/amazone-logo.png" alt="amazone logo" />
+                <img src="/dist/amazone-logo.png" alt="amazone logo" />
             </Link>
             {/* todo: Add search bar to filter shop */}
             {/* <input className="min-w-[200px] max-w-[800px] grow rounded-full min-h-[30px] max-h-[60px] justify-self-center" type="text" /> */}
